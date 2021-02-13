a4a80980d23e384708110182cc835534e0466793
amcgrat <aidan_mcgrattan@uhc.com>
2021-02-13 12:13:36 +0000

    Commit Week 4 Project 4

--- WEEK_4.py
+++ WEEK_4.py
@@ -15,3 +15,22 @@ missing_values_count = netflix_data.isnull().sum()
 #Drop rows where data is missing
 droprows= netflix_data.dropna()
 print(netflix_data.shape,droprows.shape)
+print (droprows.head())
+
+#Drop Columns where data is missing
+dropcolumns = netflix_data.dropna(axis=1)
+print(netflix_data.shape,dropcolumns.shape)
+
+#Fill all missing values with 0
+cleaned_data = netflix_data.fillna(0)
+
+#Fill all missing values to the value that comes next in the same column
+cleaned_data = netflix_data.fillna(method='bfill', axis=0).fillna(0)
+
+#Drop all rows that are duplicate
+drop_duplicates= netflix_data.drop_duplicates()
+print(netflix_data.shape,drop_duplicates.shape)
+
+#Drop Duplicate Rows based on specific columns
+drop_duplicates= netflix_data.drop_duplicates(subset=['show_id'])
+print(netflix_data.shape,drop_duplicates.shape)
