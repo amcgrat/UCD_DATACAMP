997e86e98d3f0e29247281333f51f60dd328e90a
amcgrat <aidan_mcgrattan@uhc.com>
2021-02-13 10:43:28 +0000

    Commit Week 4 Project 2

--- WEEK_4.py
+++ WEEK_4.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 
+#raad the .csv file
 netflix_data = pd.read_csv(r"C:\Users\amcgrat\\Desktop\netflix_titles.csv")
 
+#Take a first look to understand the data
+#print(netflix_data.head())
+#print(netflix_data.shape)
 
-
+#Count missing values in each column
+missing_values_count = netflix_data.isnull().sum()
+print(missing_values_count[0:])
